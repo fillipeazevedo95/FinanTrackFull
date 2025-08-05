03c462baab16b3c72e24564208beab77928b5ce5
Fillipe.Azevedo <fillipedev95@gmail.com>
2025-08-05 17:01:08 -0300

    adicionando os icones

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-9dc17825'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4edci0o3tr"
+    "revision": "0.e4qtvngnioo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
